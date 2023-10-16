355418db78759bc982ee4644c0b5978d9550873d
yantowsky <it.yantowsky@gmail.com>
2023-10-16 19:43:34 +0300

    Created HomeWork #14

---
+++ main.js
@@ -0,0 +1,9 @@
+let firtsNum = +prompt("Введіть перше число:");
+let secondNum = +prompt("Введіть друге число:");
+let thirdNum = +prompt("Введіть третьє число:");
+
+alert (`Cереднє арифметичне трьох введених чисел: ${(firtsNum+secondNum+thirdNum)/3}`);
+
+// Як що потрібно присвоїти рішенню змінну, тоді такий варіант:
+// let average = (firtsNum + secondNum + thirdNum)/3
+// alert (`Cереднє арифметичне трьох введених чисел: ${average}`);
